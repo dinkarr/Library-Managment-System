df765847c7619dfda080338c1f1e662555ab1437
dinkarr <dinkarr808@gmail.com>
2024-08-06 12:26:01 +0530

    LMS_V2 7th

--- static/components/AddBook.js
+++ static/components/AddBook.js
@@ -1,5 +1,7 @@
 export default{
     template:`
+     
+
     <div>
     <input type='text' placeholder='Book Title' v-model="resource.title">
     <input type='text' placeholder='Book Author' v-model="resource.author"></input>
@@ -14,6 +16,7 @@ export default{
 
     data(){
         return{
+            
             resource:{
                 title:null ,
                 author:null ,
